d5c99d8980ad169c058094585370915b71aebce5
Негр <137613889+Underneach@users.noreply.github.com>
2023-08-01 10:09:07 +0000

    насрал

--- modules/user_input.py
+++ modules/user_input.py
@@ -4,20 +4,20 @@ import os
 def Input():
 
     while True:
-        threads = '1'#input('\nВведите количетсво потоков: ')
+        threads = input('\nВведите количетсво потоков: ')
         if threads:
             break
         else:
             print('Введите количетсво потоков: ')
 
     while True:
-        base_file_path = 'Order12016279.txt'#input('\nВведите путь к базе с аккаунтами: ')
+        base_file_path = input('\nВведите путь к базе с аккаунтами: ')
         if os.path.exists(base_file_path):
             break
         else:
             print('Файл не найден, введите путь еще раз: ')
     while True:
-        captcha_key = 'e51ef23cb636b838cbb5a4ac6aa7dd2d'#input('\nВведите ключ от Captcha Guru: ')
+        captcha_key = input('\nВведите ключ от Captcha Guru: ')
         if captcha_key:
             break
         else:
